2b32951b361ec4b257a413cc45a8fd6f49371412
ArsalJan34 <arsaljaan532@gmail.com>
2025-10-21 00:48:47 +0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+ document.querySelectorAll('nav a').forEach(link => {
+    if (link.href === window.location.href) {
+      link.classList.add('active');
+    }
+  });
+
+
+function showSidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.classList.add('show');
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.classList.remove('show');
+}
